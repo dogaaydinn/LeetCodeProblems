cd0ae3e14792ffc9fc66e32f0c588d09da0051d6
doga <dogaa882@gmail.com>
2024-10-19 20:23:55 +0300

    solution added

---
+++ 75InterviewQuestion/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _75InterviewQuestion;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("75 Interview Questions");
+    }
+}
