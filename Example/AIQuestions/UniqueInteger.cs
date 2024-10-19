6f2e77aa94221faf50a1893ce53ce30b91d3c52f
doga <dogaa882@gmail.com>
2024-10-19 20:11:18 +0300

    example solution

---
+++ Example/AIQuestions/UniqueInteger.cs
@@ -0,0 +1,18 @@
+namespace Example.AIQuestions;
+
+public class UniqueInteger
+{
+    //All elements in an array occur three times, only one element occurs twice. Find this element.
+    public int FindDuplicate(int[] nums)
+    {
+        int seenOnce = 0, seenTwice = 0;
+        foreach (var num in nums)
+        {
+            seenOnce = ~seenTwice & (seenOnce ^ num);
+            seenTwice = ~seenOnce & (seenTwice ^ num);
+        }
+
+        return seenOnce;
+    }
+        
+}
