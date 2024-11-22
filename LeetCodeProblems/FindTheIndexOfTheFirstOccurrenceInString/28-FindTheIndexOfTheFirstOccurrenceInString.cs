20c8b6eb5006077c35a87ec02c66a86ead128557
doga <dogaa882@gmail.com>
2024-11-22 17:48:49 +0300

    Create Find first occurence solution

---
+++ LeetCodeProblems/FindTheIndexOfTheFirstOccurrenceInString/28-FindTheIndexOfTheFirstOccurrenceInString.cs
@@ -0,0 +1,17 @@
+namespace LeetCodeProblems.FindTheIndexOfTheFirstOccurrenceInString;
+
+public class _28_FindTheIndexOfTheFirstOccurrenceInString {
+    public int StrStr(string haystack, string needle) {
+        if (string.IsNullOrEmpty(needle))
+            return 0;
+
+        for (var i = 0; i <= haystack.Length - needle.Length; i++)
+        {
+            if (haystack.Substring(i, needle.Length) == needle)
+                return i; 
+        }
+
+        return -1; 
+        
+    }
+}
